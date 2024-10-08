2383087ce5f4f65dab4ffab89bb6bd2a2222cf6f
OHJAEHEON <mykd2012@gmail.com>
2024-10-08 14:12:03 +0900

    2024-10-08 ~p.265 8장 로그인구현

---
+++ src/main/java/com/itschool/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package com.itschool.springbootdeveloper.service;
+
+import com.itschool.springbootdeveloper.domain.User;
+import com.itschool.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+}
